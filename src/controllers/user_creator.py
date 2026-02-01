20edc2c8197cf055aca7641b7c2b5de65245e3da
Blenda Cunha <blenda.cunha13@gmail.com>
2026-02-01 01:25:48 -0300

    feat: Implementing user_creator with tests

--- src/controllers/user_creator.py
+++ src/controllers/user_creator.py
@@ -3,4 +3,26 @@ from src.models.repositories.interfaces.users_repository import UsersRepositoryI
 class UsarCreator:
     def __init__(self, users_repository: UsersRepositoryInterface): # Inversão da dependência - D SOLID
         self.__users_repo = users_repository
+
+    def insert_new_user(self, person_name: str, age: int, height: float) -> dict:
+        self.__check_if_user_exists(person_name)
+        self.__create_new_user(person_name, age, height)
+        return self.__formate_response()
+
+    def __check_if_user_exists(self, person_name: str) -> None:
+        select_users = self.__users_repo.select_user(person_name)
+        if (not select_users or len(select_users) == 0):
+            return
+
+        raise Exception("Usuário já cadastrado!")
+
+    def __create_new_user(self, person_name: str, age: int, height: float) -> None:
+        self.__users_repo.insert_user(person_name, age, height)
+
+    def __formate_response(self) -> dict:
+        return {
+            "Type": "Users",
+            "count": 1,
+            "message": "Usuário cadastrado com sucesso!"
+        }
     
---
+++ src/controllers/user_creator_test.py
@@ -0,0 +1,55 @@
+import pytest
+from .user_creator import UsarCreator
+
+#Classe Mock
+class UserRepositoryMock:
+    def __init__(self):
+        self.select_user_att = {}
+        self.insert_user_att = {}
+
+    def select_user(self, person_name: str) -> list:
+        self.select_user_att["person_name"] = person_name
+        return[]
+
+    def insert_user(self, person_name: str, age: int, height: float) -> None:
+        self.insert_user_att["person_name"] = person_name
+        self.insert_user_att["age"] = age
+        self.insert_user_att["height"] = height
+
+class UserRepositoryMockWithError:
+    def __init__(self):
+        self.select_user_att = {}
+
+    def select_user(self, person_name: str) -> list:
+        self.select_user_att["person_name"] = person_name
+        return[1, 2, 3]
+
+def test_insert_new_user():
+    user_repository = UserRepositoryMock()
+    user_creator = UsarCreator(user_repository)
+
+    person_name = "my_peroaijsoiajs"
+    age= 54
+    height = 1.69
+
+    response = user_creator.insert_new_user(person_name, age, height)
+
+    # print(response)
+    # print(user_repository.select_user_att)
+    assert user_repository.select_user_att["person_name"] == person_name
+    assert user_repository.insert_user_att["age"] == age
+
+    assert isinstance(response, dict)
+    assert "Type" in response
+    assert response["count"] == 1
+    assert response["message"] == "Usuário cadastrado com sucesso!"
+
+def test_insert_new_user_with_error():
+    user_repository = UserRepositoryMockWithError()
+    user_creator = UsarCreator(user_repository)
+
+    with pytest.raises(Exception) as exc_info:
+        user_creator.insert_new_user("something", 32, 1.88)
+
+    # print(exc_info.value)
+    assert str(exc_info.value) == "Usuário já cadastrado!"
